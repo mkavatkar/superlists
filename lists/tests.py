08953bc5d3483683aee9052c27998d085f28d0c7
Mihir Kavatkar <kavatkar@usc.edu>
2016-09-02 09:59:34 -0700

    Model for lists and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -27,8 +27,16 @@ class HomepageTest(TestCase):
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
         response = home_page(request)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
         self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+            )
+        self.assertEqual(response.content.decode(), expected_html)
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
