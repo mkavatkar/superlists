41f152a5935105827e9e63e551e9547e792abf9a
Mihir Kavatkar <kavatkar@usc.edu>
2016-09-02 09:51:35 -0700

    Model for lists and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 # Create your tests here.
@@ -28,3 +29,18 @@ class HomepageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The item of this list'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item of this list'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'The item of this list')
+        self.assertEqual(saved_items[1].text, 'The second item of this list')
