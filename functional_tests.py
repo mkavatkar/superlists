3139db5d68c62521c4ff82ec366626601d55f624
Mihir Kavatkar <kavatkar@usc.edu>
2016-09-02 09:07:14 -0700

    Small refactor to check rows in table dynamically

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_if_row_exists_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_later(self):
         self.browser.get("http://127.0.0.1:8000/")
 
@@ -32,15 +37,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # import time
         # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            '1: Buy a dog', [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-         )
+        self.check_if_row_exists_in_list_table('1: Buy a dog')
+        self.check_if_row_exists_in_list_table('2: Use peacock ' +
+                                               'feathers to make a fly')
 
         self.fail("Finished the test!")
 
