fc17c8bd3805fbb15dd89075d5066eaf1255d7b5
Mihir Kavatkar <kavatkar@usc.edu>
2016-09-06 10:24:36 -0700

    Updated Readme file

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,13 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    List_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=List_)
     return render(request, 'list.html', {'items': items})
 
 
